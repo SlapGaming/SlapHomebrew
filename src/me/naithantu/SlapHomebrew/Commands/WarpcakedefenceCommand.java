25b25059280f24f8d3b13bf34c389c4f44a85924
naithantu <m.tebraake.95@gmail.com>
2013-03-31 00:57:56 +0100

    Moved more stuff to Util class

--- src/me/naithantu/SlapHomebrew/Commands/WarpcakedefenceCommand.java
+++ src/me/naithantu/SlapHomebrew/Commands/WarpcakedefenceCommand.java
@@ -1,14 +1,12 @@
 package me.naithantu.SlapHomebrew.Commands;
 
 import me.naithantu.SlapHomebrew.SlapHomebrew;
+import me.naithantu.SlapHomebrew.Util;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 
 public class WarpcakedefenceCommand extends AbstractCommand {
 	public WarpcakedefenceCommand(CommandSender sender, String[] args, SlapHomebrew plugin) {
@@ -28,14 +26,14 @@ public class WarpcakedefenceCommand extends AbstractCommand {
 
 		Player player = (Player) sender;
 		if (SlapHomebrew.allowCakeTp == true) {
-			if (emptyInv()) {
+			if (Util.hasEmptyInventory(player)) {
 				player.teleport(new Location(Bukkit.getServer().getWorld("world") , 333.0, 28.0, -722.0));
 				this.msg(sender, "You have been teleported to cake defence!");
 			} else {
 				this.badMsg(sender, "Empty your inventory and take off your armor, then use /warpcakedefence again!");
 			}
 		} else {
-			if (emptyInv()) {
+			if (Util.hasEmptyInventory(player)) {
 				player.teleport(new Location(Bukkit.getServer().getWorld("world"), 333.0, 45.0, -751.0));
 				this.msg(sender, "You aren't allowed to tp to cakedefence now, you have been teleported to the spectator area!");
 			} else {
@@ -43,27 +41,5 @@ public class WarpcakedefenceCommand extends AbstractCommand {
 			}
 		}
 		return true;
-	}
-	
-	private boolean emptyInv(){
-		Boolean emptyInv = true;
-		PlayerInventory inv = ((Player) sender).getInventory();
-		for (ItemStack stack : inv.getContents()) {
-			try {
-				if (stack.getType() != (Material.AIR)) {
-					emptyInv = false;
-				}
-			} catch (NullPointerException e) {
-			}
-		}
-		for (ItemStack stack : inv.getArmorContents()) {
-			try {
-				if (stack.getType() != (Material.AIR)) {
-					emptyInv = false;
-				}
-			} catch (NullPointerException e) {
-			}
-		}
-		return emptyInv;
 	}
 }
--- src/me/naithantu/SlapHomebrew/Util.java
+++ src/me/naithantu/SlapHomebrew/Util.java
@@ -3,7 +3,11 @@ package me.naithantu.SlapHomebrew;
 import me.naithantu.SlapHomebrew.Storage.YamlStorage;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.managers.RegionManager;
@@ -31,5 +35,42 @@ public class Util {
 			}
 		}
 		return false;
+	}
+	
+	public static boolean hasEmptyInventory(Player player){
+		Boolean emptyInv = true;
+		PlayerInventory inv = player.getInventory();
+		for (ItemStack stack : inv.getContents()) {
+			//TODO Is try - catch really required here?
+			try {
+				if (stack.getType() != (Material.AIR)) {
+					emptyInv = false;
+				}
+			} catch (NullPointerException e) {
+			}
+		}
+		for (ItemStack stack : inv.getArmorContents()) {
+			try {
+				if (stack.getType() != (Material.AIR)) {
+					emptyInv = false;
+				}
+			} catch (NullPointerException e) {
+			}
+		}
+		return emptyInv;
+	}
+	
+	public static boolean hasFullInventory(Player player){
+		Boolean fullInv = true;
+		PlayerInventory inv = player.getInventory();
+		for (ItemStack stack : inv.getContents()) {
+			try {
+				if (stack.getType() == (Material.AIR)) {
+					fullInv = false;
+				}
+			} catch (NullPointerException e) {
+			}
+		}
+		return fullInv;
 	}
 }
