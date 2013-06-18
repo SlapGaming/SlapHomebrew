7ec76243c4ec6a9d31daa25383ea17d8b9b7170b
naithantu <m.tebraake.95@gmail.com>
2013-06-18 23:15:05 +0200

    Block more dispense actions

--- src/me/naithantu/SlapHomebrew/Listeners/DispenseListener.java
+++ src/me/naithantu/SlapHomebrew/Listeners/DispenseListener.java
@@ -8,7 +8,13 @@ import org.bukkit.event.block.BlockDispenseEvent;
 public class DispenseListener implements Listener {
 	@EventHandler
 	public void onBlockDispenseEvent(BlockDispenseEvent event) {
-		if (event.getItem().getType() == Material.LAVA_BUCKET || event.getItem().getType() == Material.FIREBALL) {
+		if(!event.getBlock().getWorld().getName().equals("world_creative")){
+			if(event.getItem().getType() == Material.LAVA_BUCKET || event.getItem().getType() == Material.TNT){
+				event.setCancelled(true);
+			}
+		}
+		
+		if (event.getItem().getType() == Material.FIREBALL) {
 			event.setCancelled(true);
 		}
 	}
