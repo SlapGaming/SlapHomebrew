86a064c846d75345ceb05e667c4deb681046330e
Rick Fontein <r.fontein@hotmail.com>
2013-07-03 22:09:06 +0200

    Make /world more user friendly

--- src/me/naithantu/SlapHomebrew/Commands/WorldCommand.java
+++ src/me/naithantu/SlapHomebrew/Commands/WorldCommand.java
@@ -15,20 +15,50 @@ public class WorldCommand extends AbstractCommand {
 			this.badMsg(sender, "You need to be in-game to do that!");
 			return true;
 		}
-		
+
 		if (!testPermission(sender, "world")) {
 			this.noPermission(sender);
 			return true;
 		}
-		
+
 		Player player = (Player) sender;
 
 		String worldName = player.getWorld().getName();
 		worldName = worldName.replace("world_", "");
-		if(worldName.contains("resource"))
+		if (worldName.contains("resource"))
 			worldName = "resource";
-		
-		this.msg(sender, "You are in the " + worldName.toLowerCase() + " world.");
+		switch (worldName) {
+		case "world":
+			this.msg(sender, "You are in the pre-1.6 (old) survival world.");
+			break;
+		case "survival2":
+			this.msg(sender, "You are in the new survival world.");
+			break;
+		case "start":
+			this.msg(sender, "You are in the lobby world.");
+			break;
+		case "resource":
+			this.msg(sender, "You are in the resource world.");
+			break;
+		case "pvp":
+			this.msg(sender, "You are in the PvP world.");
+			break;
+		case "the_end":
+			this.msg(sender, "You are in the end.");
+			break;
+		case "nether":
+			this.msg(sender, "You are in the nether.");
+			break;
+		case "creative":
+			this.msg(sender, "You are in the creative world.");
+			break;
+		case "sonic":
+			this.msg(sender, "You are at the Sonic the Hedgehog racetrack.");
+			break;
+		default:
+			this.msg(sender, "You are in an undefined world!");
+			break;
+		}
 		return true;
 	}
 }
