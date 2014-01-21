5fc20cab0c570762191b3be3a13d8ea5799106df
stoux2 <stoux2@gmail.com>
2014-01-21 19:34:26 +0100

    Readded SearchRegion command

--- src/me/naithantu/SlapHomebrew/Commands/Staff/SearchregionCommand.java
+++ src/me/naithantu/SlapHomebrew/Commands/Staff/SearchregionCommand.java
@@ -1,30 +1,81 @@
 package me.naithantu.SlapHomebrew.Commands.Staff;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import me.naithantu.SlapHomebrew.Commands.AbstractCommand;
 import me.naithantu.SlapHomebrew.Commands.Exception.CommandException;
-import me.naithantu.SlapHomebrew.Commands.Exception.ErrorMsg;
+import me.naithantu.SlapHomebrew.Controllers.PlayerLogging.RegionLogger;
+import me.naithantu.SlapHomebrew.Util.Util;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 public class SearchregionCommand extends AbstractCommand {
+	
 	public SearchregionCommand(CommandSender sender, String[] args) {
 		super(sender, args);
 	}
 
 	public boolean handle() throws CommandException {
 		testPermission("searchregion"); //Test perm
-		if (args.length != 1)  return false; //Check usage
+		if (args.length == 0)  return false; //Check usage
 		
-	//	String regionname = args[0].toLowerCase();
-	//	if (!plugin.getRegionMap().containsKey(regionname)) throw new CommandException("No results found!"); //Check if there are any results
+		checkDoingCommand();
 		
-	//	String[] changes = plugin.getRegionMap().get(regionname).split("<==>"); //Get results
-	//	sender.sendMessage(ChatColor.DARK_AQUA + "Region changes for region " + regionname + ":");
-	//	for (String change : changes) { //Send all
-	//		sender.sendMessage(ChatColor.GOLD + change);
-	//	}	
-	//	return true;
-		throw new CommandException(ErrorMsg.notSupportedYet);
+		final String region = args[0].toLowerCase();
+		final String world;
+		final boolean legacy;
+		
+		if (args.length == 1) { //Only Region given
+			world = getPlayer().getWorld().getName();
+			legacy = false;
+		} else if (args[args.length -1].equalsIgnoreCase("old")) { //Legacy
+			legacy = true;
+			world = null; //Compile weeh
+		} else { //Region & World given
+			legacy = false;
+			world = args[1];
+		}
+		
+		addDoingCommand();
+		
+		if (!legacy) {
+			RegionLogger.getRegionChanges(sender, region, world);
+		} else {
+			Util.runASync(plugin, new Runnable() {
+				
+				private BufferedReader bf;
+
+				@Override
+				public void run() {
+					try {
+						bf = new BufferedReader(new FileReader(plugin.getDataFolder() + File.separator + "legacyWGs.yml"));
+						String line;
+						while ((line = bf.readLine()) != null) { //Read lines
+							String[] split = line.split(":", 2); //Split on [Regionname]:[Changes]
+							if (split[0].equalsIgnoreCase(region)) { //Match regionname
+								String[] changes = split[1].split("<==>"); //Split on crazy naith thing
+								Util.msg(sender, "Changes for region " + ChatColor.GREEN + region + ChatColor.WHITE + " (Legacy File)."); //Msg 
+								for (String change : changes) { //Message changes
+									sender.sendMessage(ChatColor.GOLD + "  \u2517\u25B6 " + ChatColor.WHITE + change);
+								}
+								removeDoingCommand();
+								return;
+							}
+						}
+						bf.close();
+						Util.badMsg(sender, "No region found with this name in the legacy file.");
+					} catch (IOException e) {
+						Util.badMsg(sender, "Something went wrong with reading the file.");
+					} finally {
+						removeDoingCommand();
+					}
+				}
+			});
+		}
+		return true;
 	}
 }
--- src/me/naithantu/SlapHomebrew/Controllers/PlayerLogging/RegionLogger.java
+++ src/me/naithantu/SlapHomebrew/Controllers/PlayerLogging/RegionLogger.java
@@ -1,9 +1,19 @@
 package me.naithantu.SlapHomebrew.Controllers.PlayerLogging;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 
+import me.naithantu.SlapHomebrew.Commands.AbstractCommand;
+import me.naithantu.SlapHomebrew.Commands.Exception.CommandException;
+import me.naithantu.SlapHomebrew.Util.Util;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
@@ -14,6 +24,8 @@ public class RegionLogger extends AbstractLogger {
 	
 	private HashSet<Batchable> changes;
 	
+	private SimpleDateFormat format;
+	
 	private String sqlQuery = 
 			"INSERT INTO `mcecon`.`logger_regions` (`world`, `region`, `changed_time`, `changed_by`, `changer_is_a`, `type`, `parameter`) " +
 			"VALUES (?, ?, ?, ?, ?, ?, ?);";
@@ -22,6 +34,7 @@ public class RegionLogger extends AbstractLogger {
 		super(sql);
 		if (!enabled) return;
 		changes = new HashSet<>();
+		format = new SimpleDateFormat("dd MMM. HH:mm");
 		instance = this;
 	}
 	
@@ -72,6 +85,60 @@ public class RegionLogger extends AbstractLogger {
 		if (instance != null) instance.addChange(changedBy.getWorld().getName(), region.getId(), changedBy.getName(), changer, changeType, parameters);
 	}
 	
+	
+	public static void getRegionChanges(final CommandSender sender, final String region, final String world) throws CommandException {
+		if (instance == null) {
+			AbstractCommand.removeDoingCommand(sender);
+			throw new CommandException("The RegionLogger is currently disabled.");
+		}
+		Util.runASync(instance.plugin, new Runnable() {
+			
+			@Override
+			public void run() {
+				ArrayList<RegionChange> foundChanges = new ArrayList<>();
+				for (Batchable b : instance.changes) { //Loop thru unbatched RegionChanges
+					RegionChange change = (RegionChange) b; //Cast
+					if (change.region.equalsIgnoreCase(region) && change.world.equalsIgnoreCase(world)) { //Check if match
+						foundChanges.add(change);
+					}
+				}
+				try {
+					PreparedStatement prep = instance.sql.getConnection().prepareStatement( //Prep Statement for getting changes
+						"SELECT `changed_time`, `changed_by`, `changer_is_a`, `type`, `parameter` FROM `logger_regions` " +
+						"WHERE `world` = ? AND `region` = ?;"
+					);
+					prep.setString(1, world);
+					prep.setString(2, region);
+					ResultSet rs = prep.executeQuery(); //Get changes
+					while (rs.next()) { //Go thru results
+						foundChanges.add(instance.new RegionChange( //Add changes
+								world, region, 
+								rs.getLong(1), 
+								rs.getString(2), 
+								ChangerIsA.valueOf(rs.getString(3)), 
+								ChangeType.valueOf(rs.getString(4)), 
+								rs.getString(5))
+						);
+					}
+				} catch (SQLException | IllegalArgumentException e) { //Meh. 
+					Util.badMsg(sender, "Something went wrong. Beep bep.");
+					e.printStackTrace();
+				}
+				
+				if (foundChanges.size() > 0) { //Entries
+					Collections.sort(foundChanges); //Sort list
+					Util.msg(sender, "Changes for region " + ChatColor.GREEN + region + ChatColor.WHITE + " (world: " + ChatColor.GREEN + world + ChatColor.WHITE + ").");
+					for (RegionChange change : foundChanges) {
+						sender.sendMessage(ChatColor.GOLD + " \u2517\u25B6 " + change);
+					}
+				} else { //No changes found
+					Util.badMsg(sender, "No changes found for region " + ChatColor.YELLOW + region + ChatColor.RED + " in world " + ChatColor.YELLOW + world + ChatColor.RED + ".");
+				}
+				AbstractCommand.removeDoingCommand(sender);
+			}
+		});
+	}
+	
 	@Override
 	public void batch() {
 		batch(sqlQuery, changes);
@@ -83,7 +150,7 @@ public class RegionLogger extends AbstractLogger {
 		instance = null;
 	}
 	
-	private class RegionChange implements Batchable {
+	private class RegionChange implements Batchable, Comparable<RegionChange> {
 		
 		String world;
 		String region;
@@ -112,6 +179,47 @@ public class RegionLogger extends AbstractLogger {
 			preparedStatement.setString(5, aType.toString());
 			preparedStatement.setString(6, type.toString());
 			preparedStatement.setString(7, parameters);
+		}
+		
+		@Override
+		public int compareTo(RegionChange o) {
+			return (int) (changedTime - o.changedTime);
+		}
+		
+		@Override
+		public String toString() {
+			String s = format.format(changedTime) + " | " + ChatColor.WHITE;
+			s += changedBy + " (" + aType.toString().substring(0, 1).toUpperCase() + ") ";
+			switch (type) {
+			case addmember:
+				s += "added members: " + parameters;
+				break;
+			case addowner:
+				s += "added owners: " + parameters;
+				break;
+			case create:
+				s += "created the region.";
+				break;
+			case flag:
+				s += "added/removed a flag: " + parameters;
+				break;
+			case priority:
+				s += "changed the priority to: " + parameters;
+				break;
+			case redefine:
+				s += "redefined the region.";
+				break;
+			case remove:
+				s += "removed the region.";
+				break;
+			case removemember:
+				s += "removed members: " + parameters;
+				break;
+			case removeowner:
+				s += "removed owners: " + parameters;
+				break;
+			}
+			return s;
 		}
 				
 	}
