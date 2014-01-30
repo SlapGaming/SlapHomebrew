e9097a2ef4ec7905a48de81d2bc34cd8713cfbb3
stoux2 <stoux2@gmail.com>
2014-01-30 22:40:11 +0100

    Remove SLAP & Guide from Tab

--- src/me/naithantu/SlapHomebrew/Controllers/TabController.java
+++ src/me/naithantu/SlapHomebrew/Controllers/TabController.java
@@ -20,10 +20,8 @@ public class TabController extends AbstractController {
 	private ArrayList<String> ops;
 	private ArrayList<String> admins;
 	private ArrayList<String> mods;
-	private ArrayList<String> guides;
 	private ArrayList<String> specials;
 	private ArrayList<String> vips;
-	private ArrayList<String> slaps;
 	private ArrayList<String> members;
 	private ArrayList<String> builders;
 	
@@ -55,10 +53,8 @@ public class TabController extends AbstractController {
 		ops = new ArrayList<>();
 		admins = new ArrayList<>();
 		mods = new ArrayList<>();
-		guides = new ArrayList<>();
 		specials = new ArrayList<>();
 		vips = new ArrayList<>();
-		slaps = new ArrayList<>();
 		members = new ArrayList<>();
 		builders = new ArrayList<>();
 		
@@ -71,18 +67,16 @@ public class TabController extends AbstractController {
 		if (ops.size() > 0) tabSize = tabSize + ops.size() + 1;
 		if (admins.size() > 0) tabSize = tabSize + admins.size() + 1;
 		if (mods.size() > 0) tabSize = tabSize + mods.size() + 1;
-		if (guides.size() > 0) tabSize = tabSize + guides.size() + 1;
 		if (specials.size() > 0) tabSize = tabSize + specials.size() + 1;
 		if (vips.size() > 0) tabSize = tabSize + vips.size() + 1;
-		if (slaps.size() > 0) tabSize = tabSize + slaps.size() + 1;
 		if (members.size() > 0) tabSize = tabSize + members.size() + 1;
 		if (builders.size() > 0) tabSize = tabSize + builders.size() + 1;
 		if (tabSize == 0) return;
 		int x = 0;
 		int playersOnline = plugin.getServer().getOnlinePlayers().length;
 		String[] tab = new String[tabSize];
-		boolean fOps; boolean fAdmins; boolean fMods; boolean fGuides; boolean fSpecials; boolean fVips; boolean fSlaps; boolean fMembers; boolean fBuilders;
-		fOps = fAdmins = fMods = fGuides = fSpecials = fVips = fSlaps = fMembers = fBuilders = true;
+		boolean fOps; boolean fAdmins; boolean fMods; boolean fSpecials; boolean fVips; boolean fMembers; boolean fBuilders;
+		fOps = fAdmins = fMods = fSpecials = fVips = fMembers = fBuilders = true;
 		for (String p : ops) { 
 			if (fOps) { fOps = false; tab[x] = ChatColor.DARK_RED + "-- Owners --"; x++; }
 			tab[x] = ChatColor.DARK_RED + p; x++; 
@@ -95,10 +89,6 @@ public class TabController extends AbstractController {
 			if (fMods) { fMods = false; tab[x] = ChatColor.AQUA + "-- Mods --"; x++; }
 			tab[x] = ChatColor.AQUA + p; x++; 
 			}
-		for (String p : guides) { 
-			if (fGuides) { fGuides = false; tab[x] = ChatColor.GOLD + "-- Guides --"; x++; }
-			tab[x] = ChatColor.GOLD + p; x++; 
-			}
 		for (String p : specials) {
 			if (fSpecials) { fSpecials = false; tab[x] = ChatColor.DARK_AQUA + "-- Specials --"; x++; }
 			tab[x] = ChatColor.DARK_AQUA + p; x++;
@@ -106,10 +96,6 @@ public class TabController extends AbstractController {
 		for (String p : vips) { 
 			if (fVips) { fVips = false; tab[x] = ChatColor.BLUE + "-- VIPs --"; x++; }
 			tab[x] = ChatColor.BLUE + p; x++; 
-			}
-		for (String p : slaps) { 
-			if (fSlaps) { fSlaps = false; tab[x] = ChatColor.YELLOW + "-- SLAPs --"; x++; }
-			tab[x] = ChatColor.YELLOW + p; x++; 
 			}
 		for (String p : members) { 
 			if (fMembers) { fMembers = false; tab[x] = ChatColor.GREEN + "-- Members --"; x++; }
@@ -186,31 +172,16 @@ public class TabController extends AbstractController {
 		if (user != null) {
 			PermissionGroup[] groups = user.getGroups();
 			if (groups != null) {
-				String prefix;
-				boolean added = false;
 				if (groups.length > 0) {
 					switch (groups[0].getName()) {
 					case "builder":
 						addToList(builders, playerName);
 						break;
-					case "Member":
+					case "Member": case "Guide": case "Slap":
 						addToList(members, playerName);
 						break;
-					case "Slap":
-						addToList(slaps, playerName);
-						break;
-					case "VIP":
-						prefix = user.getPrefix();
-						if (prefix != null) {
-							if (prefix.toLowerCase().contains("slap")) {
-								addToList(slaps, playerName);
-								added = true;
-							}
-						}
-						if (!added) addToList(vips, playerName);
-						break;
-					case "Guide": case "VIPGuide":
-						addToList(guides, playerName);
+					case "VIP": case "VIPGuide":
+						addToList(vips, playerName);
 						break;
 					case "Mod":
 						addToList(mods, playerName);
@@ -226,10 +197,8 @@ public class TabController extends AbstractController {
 							switch(group) {
 							case builders:	addToList(builders, playerName);	break;
 							case members: 	addToList(members, playerName); 	break;
-							case slaps: 	addToList(slaps, playerName); 		break;
 							case vips: 		addToList(vips, playerName); 		break;
 							case specials: 	addToList(specials, playerName);	break;
-							case guides: 	addToList(guides, playerName);		break;
 							case mods: 		addToList(mods, playerName);		break;
 							case admins:	addToList(admins, playerName);		break;
 							default:		addToList(ops, playerName);
@@ -254,13 +223,11 @@ public class TabController extends AbstractController {
 			switch(x) {
 			case 1:	if (builders.remove(playerName)) removed = true; break;
 			case 2:	if (members.remove(playerName)) removed = true; break;
-			case 3:	if (slaps.remove(playerName)) removed = true; break;
-			case 4:	if (vips.remove(playerName)) removed = true; break;
-			case 5: if (specials.remove(playerName)) removed = true; break;
-			case 6:	if (guides.remove(playerName)) removed = true; break;
-			case 7:	if (mods.remove(playerName)) removed = true; break;
-			case 8:	if (admins.remove(playerName)) removed = true; break;
-			case 9:	if (ops.remove(playerName)) removed = true; break;
+			case 3:	if (vips.remove(playerName)) removed = true; break;
+			case 4: if (specials.remove(playerName)) removed = true; break;
+			case 5:	if (mods.remove(playerName)) removed = true; break;
+			case 6:	if (admins.remove(playerName)) removed = true; break;
+			case 7:	if (ops.remove(playerName)) removed = true; break;
 			default: return;
 			}
 			x++;
@@ -278,10 +245,8 @@ public class TabController extends AbstractController {
 		if (!tabApiSetup) return; //Return if not setup
 		builders.clear();
 		members.clear();
-		slaps.clear();
 		vips.clear();
 		specials.clear();
-		guides.clear();
 		mods.clear();
 		admins.clear();
 		ops.clear();
@@ -290,7 +255,7 @@ public class TabController extends AbstractController {
 	}
 	
 	public enum TabGroup {
-		builders, members, slaps, vips, specials, guides, mods, admins, ops;
+		builders, members, vips, specials, mods, admins, ops;
 	}
 	
 	/**
