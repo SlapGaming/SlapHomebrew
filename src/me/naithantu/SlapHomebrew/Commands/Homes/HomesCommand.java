b25233515470165e3e7669d5c1e364b7e924fa2e
stoux2 <stoux2@gmail.com>
2014-02-21 22:09:37 +0100

    FancyMessage implementation into /homes

--- src/me/naithantu/SlapHomebrew/Commands/Homes/HomesCommand.java
+++ src/me/naithantu/SlapHomebrew/Commands/Homes/HomesCommand.java
@@ -9,6 +9,7 @@ import org.bukkit.entity.Player;
 import me.naithantu.SlapHomebrew.Commands.AbstractCommand;
 import me.naithantu.SlapHomebrew.Commands.Exception.CommandException;
 import me.naithantu.SlapHomebrew.Controllers.Homes;
+import me.naithantu.SlapHomebrew.Controllers.FancyMessage.FancyMessage;
 import me.naithantu.SlapHomebrew.Util.Util;
 
 public class HomesCommand extends AbstractCommand {
@@ -19,17 +20,50 @@ public class HomesCommand extends AbstractCommand {
 
 	@Override
 	public boolean handle() throws CommandException {
-		Player p = getPlayer(); //Player
+		final Player p = getPlayer(); //Player
 		testPermission("home"); //Perm
 		
-		String playername = p.getName();
-		Homes homes = plugin.getHomes(); //Get Homes
+		checkDoingCommand();
 		
-		List<String> homeList = homes.getHomes(playername); //Get homes
-		int homesSize = homeList.size(); //Get size
-		hMsg("You have " + homesSize + " out of " + homes.getTotalNumberOfHomes(playername) + " homes."); //Send used homes
-		if (homesSize != 0) {
-			p.sendMessage("Homes: " + ChatColor.GRAY + Util.buildString(homeList, ChatColor.WHITE + ", " + ChatColor.GRAY)); //Send all possible homes
+		final String playername = p.getName();
+		final Homes homes = plugin.getHomes(); //Get Homes
+		
+		final List<String> homeList = homes.getHomes(playername); //Get homes
+		final int homesSize = homeList.size(); //Get size
+		
+		if (homesSize == 0) {
+			hMsg("You have 0 out of " + homes.getTotalNumberOfHomes(playername) + " homes."); //Send used homes
+		} else {
+			addDoingCommand();
+			Util.runASync(new Runnable() {
+				
+				@Override
+				public void run() {
+					FancyMessage fm = new FancyMessage(" \u2517\u25B6 ").color(ChatColor.GOLD)
+							.addText("Homes -> Click").tooltip("Click on a home to teleport to that home!")
+							.addText(": "); //Create new FancyMessage
+					
+					boolean first = true;
+					for (String home : homeList) { //Loop thru homes
+						if (!first) { //If not first add comma
+							fm.addText(", ");
+						}
+						fm.addText(home).color(ChatColor.GRAY).runCommand("/home " + home); //Add home
+						first = false;
+					}
+					
+					//Parse to JSON
+					String json = fm.toJSONString();
+					
+					//Send used homes
+					hMsg("You have " + homesSize + " out of " + homes.getTotalNumberOfHomes(playername) + " homes.");
+					//Send clickable homes
+					Util.sendJsonMessage(p, json);					
+					
+					//Remove doing command
+					removeDoingCommand();
+				}
+			});
 		}
 		return true;
 	}
